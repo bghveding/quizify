2d5ed18b1fb2ab5e7caf669a9df0175324fa6c47
Bjørnar <bjornagh@gmail.com>
2012-12-09 18:22:09 +0100

    Remove stuff, update gitignore

--- js/main-config.js
+++ js/main-config.js
@@ -1,26 +1,25 @@
 requirejs.config({
-    paths: { 
-      'cs': 'vendor/requirejs/cs',
-      'text': 'vendor/requirejs/text',
-      'underscore': 'vendor/underscore-min',
-      'underscore.string': 'vendor/underscore.string.min',
-      'backbone': 'vendor/backbone',
-      'coffee-script': 'vendor/CoffeeScript',
-    },
+  paths: { 
+    'cs': 'vendor/requirejs/cs',
+    'text': 'vendor/requirejs/text',
+    'underscore': 'vendor/underscore-min',
+    'underscore.string': 'vendor/underscore.string.min',
+    'backbone': 'vendor/backbone',
+    'coffee-script': 'vendor/CoffeeScript',
+  },
 
-    shim: {
-      'backbone': {
-          deps: ['underscore', 'jquery'],
-          exports: 'Backbone'
-      },
-      'underscore': { exports: '_' },
-      'underscore.string': {
-        deps: ['underscore'],
-        exports: '_str',
-      },
-      'vendor/bootstrap.min': ['jquery'],
-      'vendor/backbone.localStorage': ['backbone']
+  shim: {
+    'backbone': {
+        deps: ['underscore', 'jquery'],
+        exports: 'Backbone'
     },
-
-    //urlArgs: "bust=" +  (new Date()).getTime() // Avoid fetching cached version of files
+    'underscore': { exports: '_' },
+    'underscore.string': {
+      deps: ['underscore'],
+      exports: '_str',
+    },
+    'vendor/bootstrap.min': ['jquery'],
+    'vendor/backbone.localStorage': ['backbone']
+  },
+  //urlArgs: "bust=" +  (new Date()).getTime() // Avoid fetching cached version of files
 });
